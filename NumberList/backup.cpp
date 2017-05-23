e7803b7e5dc5e58aecbaf4afd229a713c88ad420
angelmarquesadacelular <angelcamposalejo@gmail.com>
2017-05-23 13:56:34 -0500

    version que suma y multiplica los elementos que se gregan

--- NumberList/backup.cpp
+++ NumberList/backup.cpp
@@ -1,7 +1,8 @@
 #include "stdafx.h"  //________________________________________ NumberList.cpp
 #include "NumberList.h"
 #include <list>
-
+double suma = 0;
+double multiplicacion = 0;
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR cmdLine, int cmdShow) {
 	NumberList app;
 	return app.BeginDialog(IDI_NumberList, hInstance);
@@ -9,50 +10,21 @@ int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR cmdLine, int cmdSho
 
 void NumberList::Window_Open(Win::Event& e)
 {
-	
+
 }
 
 void NumberList::btInsert_Click(Win::Event& e)
 {
 	//insertar elemento
 	wstring text;
-	const int number = tbxNumber.IntValue;
+	const double number = tbxNumber.DoubleValue;
 	lista.push_back(number);
 	Sys::Format(text, L"%d\r\n", number);
 	tbxList.Text += text;
 	tbxNumber.Text = L"";
 	tbxNumber.SetFocus();
-	//sacar el minimo
-	if (lista.empty() == true)
-	{
-		tbxMin.Text = L"";
-		return;
-	}
-	int minimo = lista[0];
-	unsigned int count = lista.size();
-	for (unsigned int i = 1; i < count; i++)
-	{
-		if (lista[i] < minimo)
-		{
-			minimo = lista[i];
-		}
-	}
-	tbxMin.IntValue = minimo;
-	//sacar el maximo
-	if (lista.empty() == true)
-	{
-		tbxMin.Text = L"";
-		return;
-	}
-	int maximo = lista[0];
-	unsigned int count = lista.size();
-	for (unsigned int i = 1; i < count; i++)
-	{
-		if (lista[i] > minimo)
-		{
-			maximo = lista[i];
-		}
-	}
-	tbxMin.IntValue = maximo;
+	suma += number;
+	multiplicacion *= number;
+	
 }
 
--- NumberList/backup.h
+++ NumberList/backup.h
@@ -9,7 +9,7 @@ public:
 	~NumberList()
 	{
 	}
-	vector<int>lista;
+	vector<double>lista;
 protected:
 	//______ Wintempla GUI manager section begin: DO NOT EDIT AFTER THIS LINE
 	Win::Label lb1;
@@ -17,8 +17,8 @@ protected:
 	Win::Label lb2;
 	Win::Textbox tbxNumber;
 	Win::Button btInsert;
-	Win::Textbox tbxMax;
-	Win::Textbox tbxMin;
+	Win::Textbox tbxSuma;
+	Win::Textbox tbxMultiplicacion;
 	Win::Label lb3;
 	Win::Label lb4;
 protected:
@@ -38,9 +38,9 @@ protected:
 		lb2.Create(NULL, L"New Number", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 178, 8, 83, 25, hWnd, 1002);
 		tbxNumber.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 178, 47, 88, 25, hWnd, 1003);
 		btInsert.Create(NULL, L"Insert", WS_CHILD | WS_TABSTOP | WS_VISIBLE | BS_PUSHBUTTON | BS_CENTER | BS_VCENTER, 272, 45, 70, 28, hWnd, 1004);
-		tbxMax.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_READONLY | ES_LEFT | ES_WINNORMALCASE, 252, 88, 90, 25, hWnd, 1005);
-		tbxMin.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_LEFT | ES_WINNORMALCASE, 253, 124, 89, 25, hWnd, 1006);
-		lb3.Create(NULL, L"Maximo", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 177, 90, 72, 25, hWnd, 1007);
+		tbxSuma.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_READONLY | ES_LEFT | ES_WINNORMALCASE, 252, 88, 90, 25, hWnd, 1005);
+		tbxMultiplicacion.Create(WS_EX_CLIENTEDGE, NULL, WS_CHILD | WS_TABSTOP | WS_VISIBLE | ES_AUTOHSCROLL | ES_READONLY | ES_LEFT | ES_WINNORMALCASE, 253, 124, 89, 25, hWnd, 1006);
+		lb3.Create(NULL, L"Suma", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 177, 90, 72, 25, hWnd, 1007);
 		lb4.Create(NULL, L"Minimo", WS_CHILD | WS_VISIBLE | SS_LEFT | SS_WINNORMAL, 177, 127, 74, 25, hWnd, 1008);
 		fontArial014A.Create(L"Arial", 14, false, false, false, false);
 		lb1.Font = fontArial014A;
@@ -48,8 +48,8 @@ protected:
 		lb2.Font = fontArial014A;
 		tbxNumber.Font = fontArial014A;
 		btInsert.Font = fontArial014A;
-		tbxMax.Font = fontArial014A;
-		tbxMin.Font = fontArial014A;
+		tbxSuma.Font = fontArial014A;
+		tbxMultiplicacion.Font = fontArial014A;
 		lb3.Font = fontArial014A;
 		lb4.Font = fontArial014A;
 	}
